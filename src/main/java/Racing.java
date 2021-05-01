fb2f53d457f7c53aa2cbe4c2fe818d02d60e78dc
FRABUCHI <alphasuno214@gmail.com>
2021-05-01 15:10:31 +0900

    feat: 레이스 기능 - Lab입력

--- src/main/java/Racing.java
+++ src/main/java/Racing.java
@@ -2,8 +2,18 @@ import java.util.List;
 
 public class Racing {
 
+    private final int lab;
+
+    public Racing(int lab) {
+        this.lab = lab;
+    }
+
     public Cars createCarsList(List<Car> carList) {
         Cars cars = new Cars(carList);
         return cars;
+    }
+
+    public int getLab() {
+        return lab;
     }
 }
--- src/test/java/RacingTest.java
+++ src/test/java/RacingTest.java
@@ -9,9 +9,16 @@ public class RacingTest {
 
     @Test
     void 참가자_명단_생성() {
-        Racing racing = new Racing();
+        Racing racing = new Racing(3);
         List<Car> carList = Arrays.asList(new Car("weekend"), new Car("week"), new Car("end"));
         Cars cars = racing.createCarsList(carList);
         assertEquals("weekendweekend", cars.toString());
+    }
+
+    @Test
+    void LAB_입력() {
+        Racing racing = new Racing(3);
+        int lab = racing.getLab();
+        assertEquals(3, lab);
     }
 }

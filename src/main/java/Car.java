f5e05ad71a9082ee3fa714e4bd06dc3ddfc135db
FRABUCHI <alphasuno214@gmail.com>
2021-05-01 14:21:35 +0900

    refactor: Rpm 클래스명 및하위 메소드 명 변경

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,10 +1,8 @@
-import java.util.Random;
-
 public class Car {
 
     private final int MAX_RPM = 9;
 
     public CarStatus run() {
-        return new Rpm().returnCarStatus();
+        return new Engine().start();
     }
 }
--- src/main/java/Engine.java
+++ src/main/java/Engine.java
@@ -1,17 +1,17 @@
 import java.util.Random;
 
-public class Rpm {
+public class Engine {
 
     private final int rpm;
     private static final int MIN_RPM = 4;
     private static final int MAX_RPM = 9;
 
-    public Rpm() {
+    public Engine() {
         Random random = new Random();
         this.rpm = random.nextInt(MAX_RPM);
     }
 
-    public CarStatus returnCarStatus() {
+    public CarStatus start() {
         return (rpm >= MIN_RPM) ? CarStatus.FORWARD : CarStatus.STOP;
     }
 
